7212074d6aa1e9049862f2a14e7ec3e5d04021ea
John Newbery <john@johnnewbery.com>
2019-09-04 12:14:10 -0400

    Make TestWrapper a singleton
    Trying to instantiate a new TestWrapper will just return
    the existing TestWrapper object.

--- util.py
+++ util.py
@@ -18,66 +18,84 @@ print("Source directory configured as {}".format(SOURCE_DIRECTORY))
 sys.path.insert(0, SOURCE_DIRECTORY + '/test/functional')
 from test_framework.test_framework import BitcoinTestFramework
 
-# TestWrapper utility class.
-class TestWrapper(BitcoinTestFramework):
-    """Wrapper Class for BitcoinTestFramework.
+class TestWrapper:
+    """Singleton TestWrapper class.
 
-    Provides the BitcoinTestFramework rpc & daemon process management
-    functionality to external python projects."""
+    This wraps the actual TestWrapper class to ensure that users only ever
+    instantiate a single TestWrapper."""
 
-    def set_test_params(self):
-        # This can be overriden in setup() parameter.
-        self.num_nodes=3
+    class __TestWrapper(BitcoinTestFramework):
+        """Wrapper Class for BitcoinTestFramework.
 
-    def run_test(self):
-        pass
+        Provides the BitcoinTestFramework rpc & daemon process management
+        functionality to external python projects."""
 
-    def setup(self,
-        bitcoind=os.path.abspath(SOURCE_DIRECTORY +  "/src/bitcoind"),
-        bitcoincli=None,
-        setup_clean_chain=True,
-        num_nodes=3,
-        network_thread=None,
-        rpc_timeout=60,
-        supports_cli=False,
-        bind_to_localhost_only=True,
-        nocleanup=False,
-        noshutdown=False,
-        cachedir=os.path.abspath(SOURCE_DIRECTORY + "/test/cache"),
-        tmpdir=None,
-        loglevel='INFO',
-        trace_rpc=False,
-        port_seed=os.getpid(),
-        coveragedir=None,
-        configfile=os.path.abspath(SOURCE_DIRECTORY + "/test/config.ini"),
-        pdbonfailure=False,
-        usecli = False,
-        perf = False,
-        randomseed = None):
+        def set_test_params(self):
+            # This can be overriden in setup() parameter.
+            self.num_nodes=3
 
-        self.setup_clean_chain = setup_clean_chain
-        self.num_nodes = num_nodes
-        self.network_thread = network_thread
-        self.rpc_timeout = rpc_timeout
-        self.supports_cli = supports_cli
-        self.bind_to_localhost_only = bind_to_localhost_only
+        def run_test(self):
+            pass
 
-        self.options = argparse.Namespace
-        self.options.nocleanup = nocleanup
-        self.options.noshutdown = noshutdown
-        self.options.cachedir = cachedir
-        self.options.tmpdir = tmpdir
-        self.options.loglevel = loglevel
-        self.options.trace_rpc = trace_rpc
-        self.options.port_seed = port_seed
-        self.options.coveragedir = coveragedir
-        self.options.configfile = configfile
-        self.options.pdbonfailure = pdbonfailure
-        self.options.usecli = usecli
-        self.options.perf = perf
-        self.options.randomseed = randomseed
+        def setup(self,
+            bitcoind=os.path.abspath(SOURCE_DIRECTORY +  "/src/bitcoind"),
+            bitcoincli=None,
+            setup_clean_chain=True,
+            num_nodes=3,
+            network_thread=None,
+            rpc_timeout=60,
+            supports_cli=False,
+            bind_to_localhost_only=True,
+            nocleanup=False,
+            noshutdown=False,
+            cachedir=os.path.abspath(SOURCE_DIRECTORY + "/test/cache"),
+            tmpdir=None,
+            loglevel='INFO',
+            trace_rpc=False,
+            port_seed=os.getpid(),
+            coveragedir=None,
+            configfile=os.path.abspath(SOURCE_DIRECTORY + "/test/config.ini"),
+            pdbonfailure=False,
+            usecli = False,
+            perf = False,
+            randomseed = None):
 
-        self.options.bitcoind = bitcoind
-        self.options.bitcoincli = bitcoincli
+            self.setup_clean_chain = setup_clean_chain
+            self.num_nodes = num_nodes
+            self.network_thread = network_thread
+            self.rpc_timeout = rpc_timeout
+            self.supports_cli = supports_cli
+            self.bind_to_localhost_only = bind_to_localhost_only
 
-        super(TestWrapper,self).setup()
+            self.options = argparse.Namespace
+            self.options.nocleanup = nocleanup
+            self.options.noshutdown = noshutdown
+            self.options.cachedir = cachedir
+            self.options.tmpdir = tmpdir
+            self.options.loglevel = loglevel
+            self.options.trace_rpc = trace_rpc
+            self.options.port_seed = port_seed
+            self.options.coveragedir = coveragedir
+            self.options.configfile = configfile
+            self.options.pdbonfailure = pdbonfailure
+            self.options.usecli = usecli
+            self.options.perf = perf
+            self.options.randomseed = randomseed
+
+            self.options.bitcoind = bitcoind
+            self.options.bitcoincli = bitcoincli
+
+            super().setup()
+
+    instance = None
+
+    def __new__(cls):
+        if not TestWrapper.instance:
+            TestWrapper.instance = TestWrapper.__TestWrapper()
+        return TestWrapper.instance
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+    def __setattr__(self, name):
+        return setattr(self.instance, name)
