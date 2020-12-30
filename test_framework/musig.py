71beb6c83277496a8febdcb70ac10c3f0e399bc7
Elle Mouton <elle.mouton@gmail.com>
2020-12-30 12:15:01 +0200

    Update notebook 1.2 to use Bitcoin Core v0.21

--- test_framework/musig.py
+++ test_framework/musig.py
@@ -14,16 +14,15 @@ import hashlib
 
 from .key import (
     SECP256K1,
-    SECP256K1_FIELD_SIZE,
     SECP256K1_ORDER,
-    jacobi_symbol,
+    TaggedHash,
 )
 
 def generate_musig_key(pubkey_list):
     """Aggregate individually generated public keys.
 
     Returns a MuSig public key as defined in the MuSig paper."""
-    pubkey_list_sorted = sorted([int.from_bytes(key.get_bytes()[1:], 'big') for key in pubkey_list])
+    pubkey_list_sorted = sorted([int.from_bytes(key.get_bytes(), 'big') for key in pubkey_list])
     L = b''
     for px in pubkey_list_sorted:
         L += px.to_bytes(32, 'big')
@@ -31,7 +30,7 @@ def generate_musig_key(pubkey_list):
     musig_c = {}
     aggregate_key = 0
     for key in pubkey_list:
-        musig_c[key] = hashlib.sha256(Lh + key.get_bytes()[1:]).digest()
+        musig_c[key] = hashlib.sha256(Lh + key.get_bytes()).digest()
         aggregate_key += key.mul(musig_c[key])
     return musig_c, aggregate_key
 
@@ -40,7 +39,7 @@ def aggregate_schnorr_nonces(nonce_point_list):
     R_agg = sum(nonce_point_list)
     R_agg_affine = SECP256K1.affine(R_agg.p)
     negated = False
-    if jacobi_symbol(R_agg_affine[1], SECP256K1_FIELD_SIZE) != 1:
+    if R_agg_affine[1] % 2 != 0:
         negated = True
         R_agg_negated = SECP256K1.mul([(R_agg.p, SECP256K1_ORDER - 1)])
         R_agg.p = R_agg_negated
@@ -53,13 +52,13 @@ def sign_musig(priv_key, k_key, R_musig, P_musig, msg):
     assert P_musig.compressed
     assert len(msg) == 32
     assert k_key is not None and k_key.secret != 0
-    assert jacobi_symbol(R_musig.get_y(), SECP256K1_FIELD_SIZE) == 1
+    assert R_musig.get_y() % 2 == 0
     e = musig_digest(R_musig, P_musig, msg)
     return (k_key.secret + e * priv_key.secret) % SECP256K1_ORDER
 
 def musig_digest(R_musig, P_musig, msg):
     """Get the digest to sign for musig"""
-    return int.from_bytes(hashlib.sha256(R_musig.get_x().to_bytes(32, 'big') + P_musig.get_bytes() + msg).digest(), 'big') % SECP256K1_ORDER
+    return int.from_bytes(TaggedHash("BIP0340/challenge", R_musig.get_bytes() + P_musig.get_bytes() + msg), 'big') % SECP256K1_ORDER
 
 def aggregate_musig_signatures(s_list, R_musig):
     """Construct valid Schnorr signature from a list of partial MuSig signatures."""
